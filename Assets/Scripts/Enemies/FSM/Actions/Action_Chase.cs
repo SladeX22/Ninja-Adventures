f66aeec1bc1bba5704dc5c89120313a3237a8921
Brody Rigby <rigbybc@oldcolony.info>
2024-03-20 10:38:25 -0400

    Map, Enemy Chase
    Enemy now chases player. Added to the map.

---
+++ Assets/Scripts/Enemies/FSM/Actions/Action_Chase.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action_Chase : FSM_Action
+{
+    [SerializeField] float chaseSpeed;
+    [SerializeField] float stoppingDistance;
+
+
+    EnemyBrain enemyBrain;
+
+
+
+    private void Awake()
+    {
+        enemyBrain = GetComponent<EnemyBrain>();
+    }
+
+
+    public override void Act()
+    {
+        ChasePlayer();
+    }
+
+
+    void ChasePlayer()
+    {
+        if(enemyBrain.Player == null)
+            return;
+
+        Vector3 direction = enemyBrain.Player.position - transform.position;
+
+        if(direction.magnitude >= stoppingDistance)
+            transform.Translate(direction.normalized * (chaseSpeed * Time.deltaTime));
+
+    }
+}
