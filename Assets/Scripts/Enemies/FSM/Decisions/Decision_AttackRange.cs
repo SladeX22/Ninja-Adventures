5d6ec76c456b2a2ec8b2b60aed157a49d7c891b3
Brody Rigby <rigbybc@oldcolony.info>
2024-03-25 14:19:09 -0400

    fixed sprites + attack

---
+++ Assets/Scripts/Enemies/FSM/Decisions/Decision_AttackRange.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decision_AttackRange : FSM_Decision
+{
+    [SerializeField] float attackRange;
+    [SerializeField] LayerMask playerLayer;
+
+    EnemyBrain enemyBrain;
+
+    private void Awake()
+    {
+        enemyBrain = GetComponent<EnemyBrain>();
+    }
+
+
+    public override bool Decide()
+    {
+
+        return InAttackRange();
+    }
+
+    private bool InAttackRange()
+    {
+        if(enemyBrain.Player == null)
+            return false;
+
+        Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, attackRange, playerLayer);
+
+        if (playerCollider != null)
+            return true;
+
+
+        return false;
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
