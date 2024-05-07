d8f0b87e0524e0a83b3ecd64a000dfab281380b3
Brody Rigby <rigbybc@oldcolony.info>
2024-05-07 17:50:39 -0400

    Updated Map

--- Assets/Scripts/Enemies/EnemySpawning.cs
+++ Assets/Scripts/Enemies/EnemySpawning.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class EnemySpawning : MonoBehaviour
 {
-    [SerializeField] List<GameObject> enemies;
-    [SerializeField] Player player;
+    // [SerializeField] List<GameObject> enemies;
+    // [SerializeField] int maxEnemies;
 
+    // [SerializeField] Vector2 spawnRange;
+    // [SerializeField] float spawnTime;
+    // int timeCounter;
+
+    // private void Awake() {
+    //     timeCounter = spawnTime;
+    // }
+
+    // private void Start() {
+    //     //while()
+    //     int index = Random.Range(0, enemies.Length());
+
+    //     SpawnEnemy(enemies[index]);
+    // }
+
+    // private void SpawnEnemy(GameObject go)
+    // {
+    //     Instantiate(go, new Vector3(Random.Range(0, spawnRange.x), Random.Range(0, spawnRange.y)), transform.rotation);
+    // }
 }
